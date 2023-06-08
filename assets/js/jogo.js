51688c3a584cab8290ad004f27064c31c9ee98d6
BlackStar <sthefanya86@gmail.com>
2023-06-08 18:46:38 -0300

    Retirando bug que ao clicar na linha toda a tabela sumia

--- assets/js/jogo.js
+++ assets/js/jogo.js
@@ -1,6 +1,12 @@
-let table = document.getElementById('table').addEventListener('click', (e) => {
-    box(e);
-});
+let table = document.getElementById('table');
+let bx = document.getElementsByTagName('td');
+
+for(let i = 0; i < bx.length; i++) {
+    const box = bx[i];
+    box.addEventListener('click', (e) => {
+        newGame(e)
+    });
+}
 
 function xPlayer() {
     let x = 'X';
@@ -12,6 +18,26 @@ function oPlayer() {
     return o;
 }
 
-function box(e) {
+function pointer(e) {
     let pointer = e.target;
+    console.log(pointer)
+    return pointer;
 }
+
+let game = true;
+
+function newGame(e) {
+    if(game == true) {
+        pointer(e).innerText = xPlayer();
+        game = false;
+    } else if (game == false) {
+        pointer(e).innerText = oPlayer();
+        game = true;
+    }
+}
+
+// Partes para corrigir:
+// 1. Ao clicar nas linhas o table não sumir
+// 2. Fazer identificar as formas de vitória
+// 3. Não poder mudar o simbolo
+// 4. 
